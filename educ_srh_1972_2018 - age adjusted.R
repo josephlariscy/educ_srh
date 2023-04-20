49c1a514b49b2f528d69fe3b2b154858f0740802
Joseph Lariscy (jtlriscy) <jtlriscy@memphis.edu>
2023-04-20 13:34:50 -0500

    included weights and adjusted size of labels

--- educ_srh_1972_2018 - age adjusted.R
+++ educ_srh_1972_2018 - age adjusted.R
@@ -17,7 +17,7 @@ gss_1972_2018 <- subset(gss_all,
                   year >= 1972 & year <= 2018 &
                   year != 1978 & year != 1983 & year != 1986 &
                   age >= 25,
-                  select = c(year, health, educ, sex, age))
+                  select = c(year, health, educ, sex, age, wtssall))
    # SRH wasn't measured in 1978, 1983, or 1986
    # Limit age to 25+ so respondents are old enough to complete schooling
 
@@ -67,7 +67,8 @@ F.age.mean.cen <- age - mean(age)
 F.regression <- cbind(gss.female, F.year.f, F.age.mean.cen)
 
 # Predict mean SRH for each year for women, age adjusted
-F.model.srh <- lm(health.rev ~ F.year.f + F.age.mean.cen, data = F.regression)
+F.model.srh <- lm(health.rev ~ F.year.f + F.age.mean.cen, 
+                  data = F.regression, weight = wtssall)
 summary(F.model.srh)
 
 F.pred.srh <- data.frame(F.year.f = c("1972", "1973", "1974", "1975", "1976",
@@ -89,7 +90,8 @@ F.Z.srh
 
 
 # Predict mean education for each year for women, age adjusted
-F.model.educ <- lm(educ ~ F.year.f + F.age.mean.cen, data = F.regression)
+F.model.educ <- lm(educ ~ F.year.f + F.age.mean.cen, 
+                   data = F.regression, weight = wtssall)
 summary(F.model.educ)
 
 F.pred.educ <- data.frame(F.year.f = c("1972", "1973", "1974", "1975", "1976",
@@ -122,7 +124,8 @@ M.age.mean.cen <- age - mean(age)
 M.regression <- cbind(gss.male, M.year.f, M.age.mean.cen)
 
 # Predict mean SRH for each year for women, age adjusted
-M.model.srh <- lm(health.rev ~ M.year.f + M.age.mean.cen, data = M.regression)
+M.model.srh <- lm(health.rev ~ M.year.f + M.age.mean.cen, 
+                  data = M.regression, weight = wtssall)
 summary(M.model.srh)
 
 M.pred.srh <- data.frame(M.year.f = c("1972", "1973", "1974", "1975", "1976",
@@ -144,7 +147,8 @@ M.Z.srh
 
 
 # Predict mean education for each year for men, age adjusted
-M.model.educ <- lm(educ ~ M.year.f + M.age.mean.cen, data = M.regression)
+M.model.educ <- lm(educ ~ M.year.f + M.age.mean.cen, 
+                   data = M.regression, weight = wtssall)
 summary(M.model.educ)
 
 M.pred.educ <- data.frame(M.year.f = c("1972", "1973", "1974", "1975", "1976",
@@ -170,10 +174,10 @@ M.Z.educ
 year <- as.numeric(F.mean.educ$F.year.f)
 
 fig.F.educ <- cbind(F.mean.educ, F.Z.educ, year)
-fig.F.srh  <- cbind(F.mean.srh,  F.Z.srh, year)
+fig.F.srh  <- cbind(F.mean.srh,  F.Z.srh,  year)
 
 fig.M.educ <- cbind(M.mean.educ, M.Z.educ, year)
-fig.M.srh  <- cbind(M.mean.srh, M.Z.srh, year)
+fig.M.srh  <- cbind(M.mean.srh,  M.Z.srh,  year)
 
 
 # Function to have true minus sign for negative numbers on y-axis of plots
@@ -248,7 +252,7 @@ ggplot(data = fig.M.educ, aes(x = year, y = M.Z.educ)) +
   annotate("text", y = 2.3, x = 2000, size = 4, label = "Education")
 
 
-# plot of self-rated health trend among men
+# Plot of self-rated health trend among men
 ggplot(data = fig.M.srh, aes(x = year, y = M.Z.srh)) +
   geom_line() +
   geom_point(shape = 15, size = 3) +
@@ -268,7 +272,7 @@ ggplot(data = fig.M.srh, aes(x = year, y = M.Z.srh)) +
   annotate("text", y = -1, x = 1997, size = 4, label = "Self-Rated Health")
 
 
-# Final figure: Plots of self-rated health and education by sex
+# Figure 1: Plots of self-rated health and education by sex
 
 F.educ.srh <- as.data.frame(merge(fig.F.educ, fig.F.srh, by = "year"))
 M.educ.srh <- as.data.frame(merge(fig.M.educ, fig.M.srh, by = "year"))
@@ -279,70 +283,72 @@ windows(width = 10, height = 10)
 # Women
 p1 <- ggplot(data = F.educ.srh) +
   geom_vline(xintercept = 2002.5, linetype = "dashed") +
-  geom_line(aes(y = F.Z.educ, x = year), size = 1.05) +
-  geom_line(aes(y = F.Z.srh,  x = year), size = 1.05, color = "grey") +
-  geom_point(aes(y = F.Z.educ, x = year), shape = 18, size=3) +
-  geom_point(aes(y = F.Z.srh,  x = year), shape = 15, size=3, color="grey") +
+  geom_line(aes(y = F.Z.educ, x = year), size = 1.05, color = "#003087") +
+  geom_line(aes(y = F.Z.srh,  x = year), size = 1.05, color = "#898D8D") +
+  geom_point(aes(y = F.Z.educ, x = year), shape = 18, color = "#003087", size = 3) +
+  geom_point(aes(y = F.Z.srh,  x = year), shape = 15, color = "#898D8D", size = 3) +
   coord_cartesian(ylim = c(-3, 3)) +
   scale_y_continuous(breaks = seq(-3, 3, by = 1), labels = label_true_minus) +
   scale_x_continuous(breaks = seq(1972, 2018, by = 2)) +
-  ylab(expression(bold('Standardized metric units'))) +
+  ylab(expression(bold('Standardized units'))) +
   xlab('') +
   theme(axis.line = element_line(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.background = element_blank(),
         panel.border = element_blank(),
-        axis.text.x = element_text(color = "black", size = 9,
+        axis.text.x = element_text(color = "black", size = 12,
                                    angle = 45, hjust = 1),
-        axis.text.y = element_text(color = "black", size = 10),
+        axis.text.y = element_text(color = "black", size = 12),
+        axis.title = element_text(size = 14),
         text = element_text(family = "Times New Roman"),
         legend.position = "none") +
-  annotate("text", y =  2.1, x = 2014, size = 4, label = "Education", 
+  annotate("text", y =  2.1, x = 2014, size = 14/.pt, label = "Education", 
            family = "Times New Roman") +
-  annotate("text", y = -1.5, x = 2012, size = 4, label = "Self-Rated Health", 
+  annotate("text", y = -1.5, x = 2012, size = 14/.pt, label = "Self-Rated Health", 
            family = "Times New Roman") +
-  geom_segment(x = 1977.5, xend = 1972, y = -3, yend = -3,
+  geom_segment(x = 1978.5, xend = 1972, y = -3, yend = -3,
                arrow = arrow(ends = "last", angle = 90, length=unit(.2,"cm"))) +
-  geom_segment(x = 1996.5, xend = 2002, y = -3, yend = -3,
+  geom_segment(x = 1995.5, xend = 2002, y = -3, yend = -3,
                arrow = arrow(ends = "last", angle = 90, length=unit(.2,"cm"))) +
-  annotate("text", y =-3, x = 1987, size = 3.5,
-           label = "Time period of Hill and Needham (2006) study", 
+  annotate("text", y =-3, x = 1987, size = 12/.pt,
+           label = "Period of Hill and Needham (2006) study", 
            family = "Times New Roman")
 
 
 # Men
 p2 <- ggplot(data = M.educ.srh) +
   geom_vline(xintercept = 2002.5, linetype = "dashed") +
-  geom_line(aes(y = M.Z.educ, x = year), size = 1.05) +
-  geom_line(aes(y = M.Z.srh,  x = year), size = 1.05, color = "grey") +
-  geom_point(aes(y = M.Z.educ, x = year), shape = 18, size=3) +
-  geom_point(aes(y = M.Z.srh,  x = year), shape = 15, size=3, color="grey") +
+  geom_line(aes(y = M.Z.educ, x = year), size = 1.05, color = "#003087") +
+  geom_line(aes(y = M.Z.srh,  x = year), size = 1.05, color = "#898D8D") +
+  geom_point(aes(y = M.Z.educ, x = year), shape = 18, color = "#003087", size = 3) +
+  geom_point(aes(y = M.Z.srh,  x = year), shape = 15, color = "#898D8D", size = 3) +
   coord_cartesian(ylim = c(-3, 3)) +
   scale_y_continuous(breaks = seq(-3, 3, by = 1), labels = label_true_minus) +
   scale_x_continuous(breaks = seq(1972, 2018, by = 2)) +
-  ylab(expression(bold('Standardized metric units'))) +
+  ylab(expression(bold('Standardized units'))) +
   xlab(expression(bold('Survey year'))) +
   theme(axis.line = element_line(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.background = element_blank(),
         panel.border = element_blank(),
-        axis.text.x = element_text(color = "black", size = 9,
+        axis.text.x = element_text(color = "black", size = 12,
                                    angle = 45, hjust = 1),
-        axis.text.y = element_text(color = "black", size = 10),
+        axis.text.y = element_text(color = "black", size = 12),
+        axis.title = element_text(size = 14),
         text = element_text(family = "Times New Roman"),
         legend.position = "none") +
-  annotate("text", y =  2, x = 2014, size = 4, label = "Education", 
+  annotate("text", y =  2, x = 2014, size = 14/.pt, label = "Education", 
            family = "Times New Roman") +
-  annotate("text", y = -2, x = 2010, size = 4, label = "Self-Rated Health", 
+  annotate("text", y = -2, x = 2010, size = 14/.pt, label = "Self-Rated Health", 
            family = "Times New Roman") +
-  annotate("segment", x = 1977.5, xend = 1972, y = -3, yend = -3,
+  annotate("segment", x = 1978.5, xend = 1972, y = -3, yend = -3,
            arrow = arrow(ends = "last", angle = 90, length = unit(.2,"cm"))) +
-  annotate("segment", x = 1996.5, xend = 2002, y = -3, yend = -3,
+  annotate("segment", x = 1995.5, xend = 2002, y = -3, yend = -3,
            arrow = arrow(ends = "last", angle = 90, length = unit(.2,"cm"))) +
-  annotate("text", y =-3, x = 1987, size = 3.5,
-           label = "Time period of Hill and Needham (2006) study", 
+  annotate("text", y =-3, x = 1987, size = 12/.pt,
+           label = "Period of Hill and Needham (2006) study", 
            family = "Times New Roman")
 
 figure <- ggarrange(p1, p2,
@@ -350,7 +356,8 @@ figure <- ggarrange(p1, p2,
                     hjust = c(-1.5, -2.5),
                     vjust = c(3.5, 3.5),
                     ncol = 1, nrow = 2,
-                    font.label = list(family = "Times New Roman"))
+                    font.label = list(family = "Times New Roman", size = 16))
 figure
 
-ggsave("C:/Users/jtlriscy/educ_srh/figure.png")
+ggsave("C:/Users/jtlriscy/educ_srh/figure.png", bg = "white")
+   # bg = "white" removed a thin grey line between the two plots
